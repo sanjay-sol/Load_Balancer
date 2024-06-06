c501ba8f8c17cf042852d4e54e6a51d242d4efad
Sanjay Sirangi <sanjaysirangi@gmail.com>
2024-06-07 01:00:02 +0530

    ✏️: AddNode Method

--- main.go
+++ main.go
@@ -1,38 +1,46 @@
-package main 
-
+package main
 
 import (
-  "flag"
-  "fmt"
-  "log"
-  "net/url"
+	"flag"
+	"fmt"
+	"log"
+	"net/url"
+  "strings"
 )
 
-// Node - has the data about the backend server
-
 type Node struct {
-  URL *url.URL
-  Active bool 
-  weight float64
+	URL    *url.URL
+	Active bool
 }
 
-// NodePool - Has slice of the nodes and most recently used node indexx
-
 type NodePool struct {
-  nodes []*Node 
-  current uint64
+	nodes []*Node
+}
+
+func (np *NodePool) AddNode(n *Node) {
+	np.nodes = append(np.nodes, n)
 }
 
 func main() {
 	var nodeList string
 	var port int
 	flag.StringVar(&nodeList, "nodeList", "", "List of available nodes comma-separated")
-	flag.IntVar(&port, "port", 3030, "Port to serve load-balancer")
+  flag.IntVar(&port, "port", 8080, "Port to run the load balancer")
 	flag.Parse()
+
+	nodePool := &NodePool{}
+	for _, nodeURL := range strings.Split(nodeList, ",") {
+		nodeURLParsed, err := url.Parse(nodeURL)
+		if err != nil {
+			log.Fatal(err)
+		}
+		nodePool.AddNode(&Node{
+			URL:    nodeURLParsed,
+			Active: true,
+		})
+	}
 
 	fmt.Println("Node List:", nodeList)
 	fmt.Println("Port:", port)
 	log.Println("Load Balancer started")
 }
-
-

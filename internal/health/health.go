c44681a18272064d0b4ae29a49b2d6b2fdd0d3d9
Sanjay Sirangi <sanjaysirangi@gmail.com>
2024-06-07 17:40:31 +0530

    ✍:Changed for select -  range in /health

--- internal/health/health.go
+++ internal/health/health.go
@@ -10,11 +10,8 @@ import (
 // StartHealthCheck starts periodic health checks
 func StartHealthCheck(np *balancer.NodePool) {
 	t := time.NewTicker(time.Second * 5)
-	for {
-		select {
-		case <-t.C:
-			log.Printf("Starting health check...")
-			np.HealthCheck()
-		}
+	for range t.C {
+		log.Printf("Starting health check...")
+		np.HealthCheck()
 	}
 }
